5c26bdc5e11b25aecf176a413b88d60759253d9b
saichakra-dev <gundasaichakra316@gmail.com>
2024-09-11 18:37:03 +0530

    sixth commit

--- src/app/createChallenge/page.js
+++ src/app/createChallenge/page.js
@@ -13,6 +13,7 @@ const ChallengeForm = () => {
   });
 
   const router = useRouter();
+
   const handleChange = (e) => {
     const { name, value, files } = e.target;
     setFormData({
@@ -49,74 +50,72 @@ const ChallengeForm = () => {
   };
 
   return (
-    <>
-      <div className="min-h-screen w-full flex flex-col  px-20 justify-start">
-        <form
-          onSubmit={handleSubmit}
-          encType="multipart/form-data"
-          className="w-full max-w-lg p-5"
-        >
-          <div className="flex flex-col text-xl">
-            <label className="pt-5 pb-2">Challenge Name</label>
-            <input
-              className="w-full border-2 border-slate-300 rounded-lg p-2"
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-            <label className="pt-5 pb-2">Start Date</label>
-            <input
-              className="w-full border-2 border-slate-300 rounded-lg p-2"
-              type="date"
-              name="StartDate"
-              value={formData.StartDate}
-              onChange={handleChange}
-            />
-            <label className="pt-5 pb-2">End Date</label>
-            <input
-              className="w-full border-2 border-slate-300 rounded-lg p-2"
-              type="date"
-              name="EndDate"
-              value={formData.EndDate}
-              onChange={handleChange}
-            />
-            <label className="pt-5 pb-2">Description</label>
-            <textarea
-              className="w-full border-2 border-slate-300 rounded-lg p-2 h-50"
-              name="description"
-              value={formData.description}
-              onChange={handleChange}
-            />
-            <label className="pt-5 pb-2">Image</label>
-            <input
-              className="w-full border-2 border-slate-300 rounded-lg p-2"
-              type="file"
-              name="image"
-              onChange={handleChange}
-            />
-            <label className="pt-5 pb-2">Level</label>
-            <select
-              className="w-full border-2 border-slate-300 rounded-lg p-2"
-              name="level"
-              value={formData.level}
-              onChange={handleChange}
-            >
-              <option value="">Select Level</option>
-              <option value="Easy">Easy</option>
-              <option value="Medium">Medium</option>
-              <option value="Hard">Hard</option>
-            </select>
-            <button
-              type="submit"
-              className="mt-10 bg-green-600 font-bold  text-white p-2 rounded-xl text-lg w-1/2 "
-            >
-              Create Challenge
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
+    <div className="min-h-screen w-full flex flex-col px-20 justify-start">
+      <form
+        onSubmit={handleSubmit}
+        encType="multipart/form-data"
+        className="w-full max-w-lg p-5"
+      >
+        <div className="flex flex-col text-xl">
+          <label className="pt-5 pb-2">Challenge Name</label>
+          <input
+            className="w-full border-2 border-slate-300 rounded-lg p-2"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <label className="pt-5 pb-2">Start Date</label>
+          <input
+            className="w-full border-2 border-slate-300 rounded-lg p-2"
+            type="date"
+            name="StartDate"
+            value={formData.StartDate}
+            onChange={handleChange}
+          />
+          <label className="pt-5 pb-2">End Date</label>
+          <input
+            className="w-full border-2 border-slate-300 rounded-lg p-2"
+            type="date"
+            name="EndDate"
+            value={formData.EndDate}
+            onChange={handleChange}
+          />
+          <label className="pt-5 pb-2">Description</label>
+          <textarea
+            className="w-full border-2 border-slate-300 rounded-lg p-2 h-50"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+          />
+          <label className="pt-5 pb-2">Image</label>
+          <input
+            className="w-full border-2 border-slate-300 rounded-lg p-2"
+            type="file"
+            name="image"
+            onChange={handleChange}
+          />
+          <label className="pt-5 pb-2">Level</label>
+          <select
+            className="w-full border-2 border-slate-300 rounded-lg p-2"
+            name="level"
+            value={formData.level}
+            onChange={handleChange}
+          >
+            <option value="">Select Level</option>
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
+          </select>
+          <button
+            type="submit"
+            className="mt-10 bg-green-600 font-bold text-white p-2 rounded-xl text-lg w-1/2"
+          >
+            Create Challenge
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
