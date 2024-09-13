43eb2b4c135b66070834ce7ff4762c23bc9f57cb
saichakra-dev <gundasaichakra316@gmail.com>
2024-09-13 19:49:11 +0530

    updated

--- src/app/api/challenge/route.js
+++ src/app/api/challenge/route.js
@@ -3,7 +3,6 @@ import DB from "@/app/utils/config/DB.js";
 import challengeModel from "@/app/utils/models/challengeModel";
 import path from "path";
 import { promises as fs } from "fs";
-import NextCors from "nextjs-cors";
 
 // Connect to the database
 const ConnectDb = async () => {
@@ -12,20 +11,14 @@ const ConnectDb = async () => {
   }
 };
 
-// CORS configuration
-const corsConfig = {
-  methods: ["GET", "POST"],
-  origin: "*", // Allow all origins. Change this to your specific origin if needed.
-  optionsSuccessStatus: 200,
-};
-
 // GET all challenges
-export async function GET(request) {
-  await NextCors(request, corsConfig);
+export async function GET() {
   await ConnectDb();
   try {
     const challenges = await challengeModel.find({});
-    return NextResponse.json({ success: true, challenges }, { status: 200 });
+    const response = { success: true, challenges };
+
+    return NextResponse.json(response, { status: 200 });
   } catch (error) {
     console.error("Error fetching challenges:", error);
     return NextResponse.json(
@@ -37,7 +30,6 @@ export async function GET(request) {
 
 // POST a new challenge
 export async function POST(request) {
-  await NextCors(request, corsConfig);
   await ConnectDb();
   const data = await request.formData();
   const image = data.get("image");
@@ -65,7 +57,7 @@ export async function POST(request) {
 
     const challenge = new challengeModel({
       name,
-      image: `/uploads/${image.name}`, // Fixed syntax error here
+      image: `/uploads/${image.name}`,
       description,
       StartDate,
       EndDate,
@@ -73,10 +65,9 @@ export async function POST(request) {
     });
     await challenge.save();
 
-    return NextResponse.json({
-      success: true,
-      response: "Successfully uploaded",
-    });
+    const response = { success: true, response: "Successfully uploaded" };
+
+    return NextResponse.json(response);
   } catch (error) {
     console.error("Error uploading challenge:", error);
     return NextResponse.json({
